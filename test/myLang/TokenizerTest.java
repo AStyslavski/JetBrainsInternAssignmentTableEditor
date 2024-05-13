ab33a2ae6a0f328bf29219585723d82493683922
alans <a.a.styslavski@student.tudelft.nl>
2024-05-14 00:25:22 +0200

    Tokenizer test

--- test/myLang/TokenizerTest.java
+++ test/myLang/TokenizerTest.java
@@ -10,11 +10,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class TokenizerTest {
 
     @Test
-    void tokenize() {
+    void testTokenize() {
         assertEquals(
                 List.of(new IdT("hello"), new OpenParenT(), new CellRefT("A12"), new PlusT(), new OpenParenT(),
                         new NumT(2), new MinusT(), new IdT("abs"), new OpenParenT(), new MinusT(),
                         new NumT(21), new CloseParenT(), new CloseParenT(), new CloseParenT()).toString(),
                 Tokenizer.tokenize("hello(A12 + (2 - abs(-21)))").toString());
+        assertEquals(
+                Tokenizer.tokenize("hello(A12+(2-abs(-21)))").toString(),
+                Tokenizer.tokenize("hello( A12   +  ( 2   -  abs(  -21 ) ) )").toString());
+        assertEquals(
+                List.of(new IdT("pow"), new OpenParenT(), new MinusT(), new NumT(2), new CommaT(), new CellRefT("A1"),
+                        new MinusT(), new NumT(3), new CloseParenT(), new MultT(), new OpenParenT(), new NumT(42),
+                        new PlusT(), new CellRefT("B2"), new CloseParenT()).toString(),
+                Tokenizer.tokenize("pow(-2, A1 - 3) * (42 + B2)").toString());
     }
 }
